1fdb65c9f7f0564eb47579914150f07fa7a7817f
joaovictor3g <jvdias79797@gmail.com>
2023-01-16 22:10:55 -0300

    feat: initial setup (mobile)

---
+++ mobile/App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) return <Loading />;
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#09090a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
